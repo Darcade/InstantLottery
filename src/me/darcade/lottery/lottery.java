b7771d74e76a8019eb0884e836cf2d7d49c282aa
darcade <darcade@localhost.localdomain>
2013-08-20 13:16:32 +0200

    added config

--- src/me/darcade/lottery/lottery.java
+++ src/me/darcade/lottery/lottery.java
@@ -1,9 +1,9 @@
 package me.darcade.lottery;
 
+import java.util.List;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,9 +11,11 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class lottery extends JavaPlugin{
-	int[] blacklist = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40};
-	
+public class lottery extends JavaPlugin {
+	int[] blacklist = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+			14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+			31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+
 	@Override
 	public void onDisable() {
 		System.out.println("Lottery plugin disabled!");
@@ -22,87 +24,99 @@ public class lottery extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		PluginDescriptionFile descFile = this.getDescription();
+
+		this.createConfig();
+
 		System.out.println("Lottery plugin enabled!");
 		System.out.println("Plugin Version: " + descFile.getVersion());
 	}
-	
-	
-	private boolean checkforblacklist(int vartocheck){
+
+	private boolean checkforblacklist(int vartocheck) {
+		List<Integer> blacklist = this.getConfig().getIntegerList(
+				"itemblacklist");
+
 		boolean output = false;
-		
-	    for(int k : blacklist){
-	    	if (vartocheck == k){
-	    		output = true;
-	    	}
-	    }
-		
-		
+
+		for (int k : blacklist) {
+			if (vartocheck == k) {
+				output = true;
+			}
+		}
+
 		return output;
 	}
-	
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args){
+
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String cmdLabel, String[] args) {
+
+		int maxprice = this.getConfig().getInt("max-price");
+		int itemtopay = this.getConfig().getInt("itemtopay");
+		int amounttopay = this.getConfig().getInt("amounttopay");
 		
-		
-		
+		// Item to pay
+		ItemStack itemstack = new ItemStack(itemtopay,
+				amounttopay);
+
 		boolean returnvar = false;
-		
-		Player p = (Player)sender;
-		
-		if(cmd.getName().equalsIgnoreCase("lottery")){
-			if (p.hasPermission("lottery")){
-			if(args.length == 0){
-				if(p.getInventory().contains(266)){
-					//AIR
-					ItemStack airstack = new ItemStack(190);
-					
-					//amount of gold
-					ItemStack itemstack = new ItemStack(Material.GOLD_INGOT, 1);
-					
-				    int randomNum = new Random().nextInt((421 + 1) - 1) + 1;
-				    int randomAmount = new Random().nextInt((64 + 1) - 1) + 1;
 
-				    //won item
-				    ItemStack wonitem = new ItemStack(randomNum, randomAmount);
-				    
-				    while(wonitem.getType() == airstack.getType() | this.checkforblacklist(wonitem.getTypeId())){
-						//amount of gold
-						itemstack = new ItemStack(Material.GOLD_INGOT, 1);
-						
-					    randomNum = new Random().nextInt((421 + 1) - 1) + 1;
-					    randomAmount = new Random().nextInt((64 + 1) - 1) + 1;
-					    
-					    //won item
-					    wonitem = new ItemStack(randomNum, randomAmount);
+		Player p = (Player) sender;
 
-				    }
-				    if(wonitem.getMaxStackSize() == 1){
-					    wonitem = new ItemStack(randomNum, 1);
+		if (cmd.getName().equalsIgnoreCase("lottery")) {
+			if (p.hasPermission("lottery")) {
+				if (args.length == 0) {
+					if (p.getInventory().containsAtLeast(itemstack, amounttopay)) {
+						// AIR
+						ItemStack airstack = new ItemStack(190);
+
+
+
+						int randomNum = new Random().nextInt((421 + 1) - 1) + 1;
+						int randomAmount = new Random()
+								.nextInt((maxprice + 1) - 1) + 1;
+
+						// won item
+						ItemStack wonitem = new ItemStack(randomNum,
+								randomAmount);
+
+						while (wonitem.getType() == airstack.getType()
+								| this.checkforblacklist(wonitem.getTypeId())) {
+							// amount of gold
+							itemstack = new ItemStack(itemtopay, amounttopay);
+
+							randomNum = new Random().nextInt((421 + 1) - 1) + 1;
+							randomAmount = new Random()
+									.nextInt((maxprice + 1) - 1) + 1;
+
+							// won item
+							wonitem = new ItemStack(randomNum, randomAmount);
+
+						}
+
 						p.getInventory().removeItem(itemstack);
-						p.sendMessage(ChatColor.GREEN + "Gold wurde abgenommen.");
+						p.sendMessage(ChatColor.GREEN
+								+ itemstack.getType().toString()
+								+ " wurde abgenommen.");
 						p.sendMessage("Du bekommst: " + wonitem.getType());
 						p.getInventory().addItem(wonitem);
-				    }
-				    else {
-					p.getInventory().removeItem(itemstack);
-					p.sendMessage(ChatColor.GREEN + "Gold wurde abgenommen.");
-					p.sendMessage("Du bekommst: " + wonitem.getType());
-					p.getInventory().addItem(wonitem);
-				    }
+
+					}
+
+					else {
+						p.sendMessage(ChatColor.RED
+								+ "Du hast kein " + itemstack.getType().toString() + " im Inventar");
+					}
+					returnvar = true;
 				}
-					
-				else {
-					p.sendMessage(ChatColor.RED + "Du hast kein Gold im Inventar");
-				}
-				returnvar = true;
+				returnvar = false;
 			}
-			returnvar = false;
-		}
 			returnvar = false;
 		}
 		return returnvar;
 	}
-	
-	
-	
+
+	public void createConfig() {
+		this.saveDefaultConfig();
+		System.out.println("checking config...");
+	}
+
 }
